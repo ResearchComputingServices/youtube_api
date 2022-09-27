3e933721485a616de8482bbd84f3c5191db2bf62
Jazmin Romero <jazminromero@Jazmins-MacBook-Pro.local>
2022-09-27 11:16:43 -0400

    Retrieve comments from video

--- mainOauth.py
+++ mainOauth.py
@@ -125,7 +125,7 @@ def get_video_transcript(videoId):
     return transcript_data
 
 
-def create_dict(item):
+def create_video_metadata(item):
 
     try:
         now = datetime.datetime.now()
@@ -200,7 +200,7 @@ def create_dict(item):
     return metadata
 
 
-def get_playlist_info(youtube, playlist):
+def get_playlist_metadata(youtube, playlist):
 
     # Builds a service object. In this case, the service is youtube api, version v3, with the api_key
     #youtube = build('youtube', 'v3', developerKey=api_key)
@@ -217,7 +217,7 @@ def get_playlist_info(youtube, playlist):
         requestVideosList = youtube.playlistItems().list(
             part='contentDetails',
             playlistId=playlist,
-            maxResults=5,
+            maxResults=20,
             pageToken=nextPageToken
         )
         responseVideosList = requestVideosList.execute()
@@ -238,7 +238,7 @@ def get_playlist_info(youtube, playlist):
 
 
         for item in videos_response['items']:
-            metadata = create_dict(item)
+            metadata = create_video_metadata(item)
             print ('Video {}'.format(count))
             pprint.pprint(metadata)
             print ('\n')
@@ -254,11 +254,11 @@ def get_playlist_info(youtube, playlist):
             break;
 
     #Export info to excel
-    export_dict_to_excel(records, 'info.xlsx')
+    export_dict_to_excel(records, 'metadata.xlsx')
 
 
 
-def get_video_info(youtube, video_id):
+def get_video_metadata(youtube, video_id):
 
     records ={}
 
@@ -271,7 +271,7 @@ def get_video_info(youtube, video_id):
     videos_response = videos_request.execute()
 
     for item in videos_response['items']:
-        metadata = create_dict(item)
+        metadata = create_video_metadata(item)
         pprint.pprint(metadata)
         print ('\n')
 
@@ -282,7 +282,251 @@ def get_video_info(youtube, video_id):
 
 
 
-def run_service():
+def get_comments_count(youtube,video_id):
+
+    videos_request = youtube.videos().list(
+        part="statistics",
+        id=video_id
+    )
+
+    videos_response = videos_request.execute()
+    commentsCount = 0
+
+    for item in videos_response['items']:
+        if "statistics" in item:
+            commentsCount = item["statistics"].get("commentCount", "N/A")
+
+    return commentsCount
+    print ("Total comments: " + commentsCount)
+
+
+
+
+def get_comments_for_comment(youtube, parent_id):
+
+    nextPageToken = None
+    pages = 0
+
+    list =[]
+    try:
+        while True:
+
+            pages = pages + 1
+
+            # List maxResults videos in a playlist
+            requestCommentsList = youtube.comments().list(
+                part='id,snippet',
+                parentId = parent_id,
+                maxResults=3,   #Maximum is 100
+                pageToken=nextPageToken
+            )
+
+            responseCommentsList = requestCommentsList.execute()
+            list.extend(responseCommentsList['items'])
+
+            nextPageToken = responseCommentsList.get('nextPageToken')
+            if not nextPageToken or pages == 2:
+            #if not nextPageToken:
+                break;
+    except:
+        print("Error on getting replies to comment: " + parent_id)
+        print(sys.exc_info()[0])
+        traceback.print_exc()
+
+    return list
+
+
+
+def create_comment_dict(records, item):
+
+    count = len(records)+1
+    metadata={
+        "id": "",
+        "type":"",
+        "Recipient (video or comment)":"",
+        "video url" : "",
+        "comment":"",
+        "likeCount": "",
+        "publishedAt": "",
+        "totalReplyCount": "",
+        "authorDisplayName": "",
+        "authorProfileImageUrl": "",
+        "authorChannelId": "",
+        "authorChannelUrl" :""
+    }
+
+    if "snippet" in item:
+        try:
+            metadata["id"] = item["id"]
+            metadata["type"] = "Comment to video"
+            metadata["Recipient (video or comment)"] = item["snippet"].get("videoId","")
+            url = "https://youtu.be/" + item["id"]
+            metadata["video url"] = url
+            metadata["comment"] = item["snippet"]["topLevelComment"]["snippet"].get("textDisplay","")
+            metadata["authorDisplayName"] = item["snippet"]["topLevelComment"]["snippet"].get("authorDisplayName", "")
+            metadata["authorProfileImageUrl"] = item["snippet"]["topLevelComment"]["snippet"].get("authorProfileImageUrl",                                                                                         "")
+            metadata["authorChannelId"] = item["snippet"]["topLevelComment"]["snippet"]["authorChannelId"].get("value","")
+            metadata["authorChannelUrl"] = item["snippet"]["topLevelComment"]["snippet"].get("authorChannelUrl","")
+            metadata["likeCount"] = item["snippet"]["topLevelComment"]["snippet"].get("likeCount", "")
+            metadata["publishedAt"] = item["snippet"]["topLevelComment"]["snippet"].get("publishedAt", "")
+            totalReplies = item["snippet"].get("totalReplyCount", "0")
+            metadata["totalReplyCount"] = totalReplies
+            records[count] = metadata
+
+
+
+            if "replies" in item:
+
+                if len(item["replies"])<int(totalReplies):
+                    #Get all replies
+                    print ("Getting all replies")
+                    replies = get_comments_for_comment(youtube, item["id"])
+                else:
+                    replies = item["replies"]["comments"]
+
+                print ("Total replies: " + str(len(replies)))
+
+                for reply in replies:
+
+                    metadata = {}
+                    count = count + 1
+                    metadata["id"] = reply["id"]
+                    metadata["type"] = "Reply to comment"
+                    metadata["Recipient (video or comment)"] = reply["snippet"].get("parentId", "")
+                    metadata["comment"] = reply["snippet"].get("textDisplay", "")
+                    metadata["authorChannelId"] = reply["snippet"]["authorChannelId"].get("value", "")
+                    metadata["authorChannelUrl"] = reply["snippet"].get("authorChannelUrl", "")
+                    metadata["authorDisplayName"] = reply["snippet"].get("authorDisplayName", "")
+                    metadata["authorProfileImageUrl"] = reply["snippet"].get("authorProfileImageUrl", "")
+                    metadata["likeCount"] = reply["snippet"].get("likeCount", "")
+                    metadata["publishedAt"] = reply["snippet"].get("publishedAt", "")
+                    metadata["totalReplyCount"] = "N/A"
+                    metadata["video url"]= ""
+
+                    records[count] = metadata
+        except:
+            print("Error on creating comment dictionary for video: " + item["id"])
+            print(sys.exc_info()[0])
+            traceback.print_exc()
+
+    else:
+        records[count] = metadata
+
+    return records
+
+
+def get_comments_for_video(youtube, video_id, records=None):
+
+    #commentsCount = get_comments_count(youtube,video_id)
+    #print ('Comments count: ' + str(commentsCount))
+
+    if not records:
+        records ={}
+
+    nextPageToken = None
+    pages = 0
+    count = 0
+
+
+    try:
+        while True:
+
+            pages = pages + 1
+
+            # List maxResults videos in a playlist
+            requestCommentsList = youtube.commentThreads().list(
+                part='id,snippet,replies',
+                videoId=video_id,
+                maxResults=10,    #maxResults = 100
+                pageToken=nextPageToken
+            )
+
+            responseCommentsList = requestCommentsList.execute()
+
+            for item in responseCommentsList['items']:
+                count = count + 1
+                print('Comment {}'.format(count))
+                records = create_comment_dict(records, item)
+
+
+            nextPageToken = responseCommentsList.get('nextPageToken')
+
+            if not nextPageToken or pages == 1:
+            #if not nextPageToken:
+                break;
+    except:
+        print("Error on getting video comments: " + video_id)
+        print(sys.exc_info()[0])
+        traceback.print_exc()
+
+    return records
+
+
+def export_to_excel(records, directory, name):
+    abs_path = pathlib.Path().resolve()
+    full_path = os.path.join(abs_path, directory)
+    filename = secure_filename(name)
+    file_path = os.path.join(full_path, filename)
+    export_dict_to_excel(records, file_path)
+
+
+
+
+
+
+def get_playlist_comments(youtube, playlist):
+
+    records ={}
+    nextPageToken = None
+    pages = 0
+
+
+    videos_ids = []
+    while True:
+
+        pages = pages + 1
+        # List maxResults videos in a playlist
+        requestVideosList = youtube.playlistItems().list(
+            part='contentDetails',
+            playlistId=playlist,
+            maxResults=3,  #max is 50
+            pageToken=nextPageToken
+        )
+
+        responseVideosList = requestVideosList.execute()
+
+        # Obtain video_id for each video in the response
+        for item in responseVideosList['items']:
+            videoId = item['contentDetails']['videoId']
+            videos_ids.append(videoId)
+        nextPageToken = responseVideosList.get('nextPageToken')
+
+        if not nextPageToken or pages == 2:
+        #if not nextPageToken:
+            break;
+
+
+
+    for video_id in videos_ids:
+        print ("Fetching comments for video " + video_id + "\n")
+        #To export all the comments for all the videos in the same excel file
+        records = get_comments_for_video(youtube,video_id,records)
+
+
+        #To export the comments of a video to a single file
+        #records = get_comments_for_video(youtube, video_id)
+        #if len(records)>0:
+        #    name = video_id + '.xlsx'
+        #    export_to_excel(records, 'comments', name)
+
+
+
+    # Export info to excel
+    export_dict_to_excel(records, 'comments.xlsx')
+
+
+
+def build_service_oauth():
 
     credentials = None
     # token.pickle stores the user's credentials from previously successful logins
@@ -295,7 +539,15 @@ def run_service():
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             print('Refreshing Access Token...')
-            credentials.refresh(Request())
+            try:
+                credentials.refresh(Request())
+            except:
+                if os.path.exists('token.pickle'):
+                    os.remove('token.pickle')
+                    print ("Please run the app again.")
+                else:
+                    print ("Unexpected error when attempting to login")
+
         else:
             print('Fetching New Tokens...')
             flow = InstalledAppFlow.from_client_secrets_file(
@@ -315,22 +567,40 @@ def run_service():
                 print('Saving Credentials for Future Use...')
                 pickle.dump(credentials, f)
 
-
-
+    # Builds a service object. In this case, the service is youtube api, version v3, with OAuth credentials
     youtube = build('youtube', 'v3', credentials=credentials)
     return youtube
 
-if __name__ == "__main__":
+
+def build_service_api_key():
     api_key = get_api_key()
+
+    # Builds a service object. In this case, the service is youtube api, version v3, with the api_key
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    return youtube
+
+
+if __name__ == "__main__":
+
     playlist = get_url_playlist()
-    youtube = run_service()
+    #youtube = build_service_oauth()
+    youtube  = build_service_api_key()
 
     #video_id = '1aBWct8VBXE'
     #get_video_info(youtube, video_id)
 
+    #video_id = "xrN6KtqaCTk"
+    #video_id = "rwyNJs7-pfM"
+    #video_id = "XTjtPc0uiG8"
+    #video_id = "3Wf29RiKp70"
+    #r = get_comments_for_video(youtube,video_id)
+    #export_to_excel(r, "comments", video_id+'.xlsx')
+
     if youtube:
         if playlist:
-            get_playlist_info(youtube, playlist)
+            #get_playlist_metadata(youtube, playlist)
+            get_playlist_comments(youtube, playlist)
         else:
             print ("Verify format of YouTube playlist.")
     else:
